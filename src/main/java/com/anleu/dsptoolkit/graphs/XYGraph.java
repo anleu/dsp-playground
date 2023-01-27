9e8eaea578ea1a01872e54bb199200c4e059a7fd
Andy <a.leuckert@gmail.com>
2023-01-27 21:25:17 +0100

    add first classes

---
+++ src/main/java/com/anleu/dsptoolkit/graphs/XYGraph.java
@@ -0,0 +1,19 @@
+package com.anleu.dsptoolkit.graphs;
+
+import com.sun.istack.internal.Nullable;
+
+public class XYGraph {
+
+    @Nullable
+    private double[] xValues;
+    private double[] yValues;
+
+    public XYGraph( double[] xValues, double[] yValues) {
+        this.xValues = xValues;
+        this.yValues = yValues;
+    }
+
+
+
+
+}
