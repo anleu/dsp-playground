bb20cffe3faa57704378ba788d4ce758ea966207
Andy <a.leuckert@gmail.com>
2023-01-27 21:47:02 +0100

    add simple xygraph

--- src/main/java/com/anleu/dsptoolkit/MainClass.java
+++ src/main/java/com/anleu/dsptoolkit/MainClass.java
@@ -1,11 +1,17 @@
 package com.anleu.dsptoolkit;
 
+import com.anleu.dsptoolkit.generators.SinusGenerator;
+import com.anleu.dsptoolkit.graphs.XYGraph;
+
 public class MainClass {
 
 
     public static void main(String[] args) {
 
+
+        // Show simple graph
+        double[] sinusWave = SinusGenerator.createSinusWave(1/5.0, 10000, 4);
+        XYGraph graph = new XYGraph("Sinus", null, sinusWave);
+        graph.showGraph();
     }
-
-
 }
--- src/main/java/com/anleu/dsptoolkit/graphs/XYGraph.java
+++ src/main/java/com/anleu/dsptoolkit/graphs/XYGraph.java
@@ -1,19 +1,55 @@
 package com.anleu.dsptoolkit.graphs;
 
-import com.sun.istack.internal.Nullable;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 
-public class XYGraph {
+/**
+ * Show a simple xy graph based on jfreechart
+ * Currently no options as parameter but there are a lot of options in the jfreechart library itself you can call from the context menu within the graph
+ */
+public class XYGraph extends ApplicationFrame {
 
-    @Nullable
-    private double[] xValues;
-    private double[] yValues;
 
-    public XYGraph( double[] xValues, double[] yValues) {
-        this.xValues = xValues;
-        this.yValues = yValues;
+    final XYSeries series = new XYSeries("Data");
+
+    public XYGraph(String title, double[] xValues, double[] yValues) {
+        super(title);
+        if (xValues != null) {
+            for (int i = 0; i < xValues.length; i++) {
+                series.add(xValues[i], yValues[i]);
+            }
+        } else {
+            for (int i = 0; i < yValues.length; i++) {
+                series.add(i, yValues[i]);
+            }
+        }
+
     }
 
+    public void showGraph() {
+        final XYSeriesCollection data = new XYSeriesCollection(series);
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+                "XY Series Demo",
+                "X",
+                "Y",
+                data,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
 
-
-
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        setContentPane(chartPanel);
+        pack();
+        RefineryUtilities.centerFrameOnScreen(this);
+        setVisible(true);
+    }
 }
